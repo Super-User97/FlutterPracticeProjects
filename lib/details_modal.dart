11c75d8b1a3d1bb7e6aa6a8dbc6e61e101752ce2
Super-User97 <150774239+Super-User97@users.noreply.github.com>
2023-11-15 13:14:58 +0530

    second commit

--- lib/details_modal.dart
+++ lib/details_modal.dart
@@ -1,5 +1,3 @@
-// details_modal.dart
-
 import 'package:flutter/material.dart';
 
 class DetailsModal extends StatelessWidget {
@@ -10,18 +8,82 @@ class DetailsModal extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              "Selected Name: $name",
-              style: TextStyle(fontSize: 20),
-            ),
-            // Add more details or actions as needed
-          ],
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.teal,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
         ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            width: 40,
+            height: 5,
+            color: Colors.white,
+            margin: EdgeInsets.only(bottom: 20),
+          ),
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 40,
+            child: Icon(
+              Icons.person,
+              size: 50,
+              color: Colors.teal,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            "Contact Details",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 8),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              name,
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal),
+            ),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Close"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue.shade500, // Change the button color here
+                ),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  // Add your action here
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue.shade500, // Change the button color here
+                ),
+                child: Text("Custom Action"),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
