9f1dbf1d0368e660d3d921b413a521620cbc7e69
Super-User97 <150774239+Super-User97@users.noreply.github.com>
2023-11-15 12:18:20 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'details_modal.dart'; // Import the details modal
 import 'details_page.dart';
+import 'dart:math';
 
 void main() {
   runApp(FlutterApp());
@@ -19,16 +20,60 @@ class FlutterApp extends StatelessWidget {
 }
 
 class DashBoardScreen extends StatelessWidget {
-  var arrNames = ['Raman', 'Ramanujan', 'Rajesh', 'James', 'John', 'Jacob', 'Kiwi', 'Ally', 'Ritik', 'Ronak'];
+  var arrNames = [
+    'Raman',
+    'Ramanujan',
+    'Rajesh',
+    'James',
+    'John',
+    'Jacob',
+    'Kiwi',
+    'Ally',
+    'Ritik',
+    'Ronak'
+  ];
+  // Function to generate a random gender
+  String getRandomGender() {
+    Random random = Random();
+    return random.nextBool() ? 'Male' : 'Female';
+  }
+
+  // Function to get the appropriate avatar icon based on gender
+  IconData getAvatarIcon(String gender) {
+    return gender == 'Male' ? Icons.face : Icons.pregnant_woman;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Contacts"),
+        title: Text(
+          "Contacts",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.teal,
+                Colors.blue
+              ], // Customize your gradient colors
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
       ),
       body: ListView.separated(
         itemBuilder: (context, index) {
+          Random random = Random();
+          int timeDifference = random.nextInt(51) + 10;
+          // Get a random gender (Male/Female)
+          String gender = getRandomGender();
           return InkWell(
             onTap: () {
               // Navigate to the details page when a name is tapped
@@ -43,21 +88,51 @@ class DashBoardScreen extends StatelessWidget {
               // Show the details modal when a name is long-pressed
               showModalBottomSheet(
                 context: context,
-                builder: (BuildContext context) => DetailsModal(name: arrNames[index]),
+                builder: (BuildContext context) =>
+                    DetailsModal(name: arrNames[index]),
               );
             },
             child: Ink(
               color: Colors.transparent, // Set the background color
               child: ListTile(
                 leading: CircleAvatar(
-                  child: Text('${index + 1}'),
+                  child: Icon(
+                    getAvatarIcon(gender),
+                    size: 30,
+                    color: Colors.white,
+                  ),
+                  backgroundColor: gender == 'Male' ? Colors.blue : Colors.pink,
                 ),
                 title: Text(
                   arrNames[index],
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700, color: Colors.teal),
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      color: Colors.teal),
                 ),
-                subtitle: Text('Number'),
-                trailing: Icon(Icons.add, color: Colors.teal, size: 30),
+                subtitle: Row(
+                  children: <Widget>[
+                    // Incoming/Outgoing icon based on your condition
+                    Icon(
+                      Icons
+                          .call_received, // Replace with your icon for incoming/outgoing
+                      color: Colors.green, // Customize the color
+                    ),
+                    SizedBox(width: 5), // Add some spacing
+
+                    // Mobile icon
+                    Text('Mobile', style: TextStyle(color: Colors.teal)),
+                    SizedBox(width: 5),
+
+                    // Dot separator
+                    Text('·', style: TextStyle(fontSize: 20)),
+                    SizedBox(width: 5),
+
+                    // Time (increasing dynamically)
+                    Text('$timeDifference min ago'),
+                  ],
+                ),
+                trailing: Icon(Icons.call, color: Colors.teal, size: 30),
               ),
             ),
           );
@@ -65,7 +140,7 @@ class DashBoardScreen extends StatelessWidget {
         itemCount: arrNames.length,
         separatorBuilder: (context, index) {
           return Divider(
-            height: 100,
+            height: 50,
             thickness: 3,
           );
         },
