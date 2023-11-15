2327ca0b53016b1cb0ef9063072938169db7ce47
Super-User97 <150774239+Super-User97@users.noreply.github.com>
2023-11-15 14:28:07 +0530

    updated the page details

--- lib/details_page.dart
+++ lib/details_page.dart
@@ -1,6 +1,5 @@
-// details_page.dart
-
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class DetailsPage extends StatelessWidget {
   final String name;
@@ -13,10 +12,77 @@ class DetailsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Details"),
       ),
-      body: Center(
-        child: Text(
-          "Selected Name: $name",
-          style: TextStyle(fontSize: 24),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: NetworkImage(
+                "https://img.freepik.com/free-photo/guy-shows-document-girl-group-young-freelancers-office-have-conversation-working_146671-13569.jpg?w=826&t=st=1700038271~exp=1700038871~hmac=6b411dc4ccd6971e774162c1335757b55876db3105c7d1988118969a34e620c1"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+          child: Container(
+            color: Colors.transparent,
+            padding: EdgeInsets.all(16),
+            child: Card(
+              elevation: 8,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              color: Colors.transparent,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.teal, Colors.blue],
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.white,
+                      child: Icon(
+                        Icons.person,
+                        size: 60,
+                        color: Colors.teal,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "Contact Name: $name",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    // Add more details or actions as needed
+                    Text(
+                      "Phone Number: +91 1234567890",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Email: example@example.com",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ),
     );
